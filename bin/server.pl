46e5e13c55786f1626ae49d58f4672b16867ed5c
Luke Schierer <lschiere@amazon.com>
2025-12-31 17:14:14 -0500

    let just build command work

--- bin/server.pl
+++ bin/server.pl
@@ -10,98 +10,102 @@ use PAGI::Server;
 use Future::AsyncAwait;
 use Path::Tiny;
 
-my $framework = PAGI::WebServer->new(
-    config_file => 'config.yml'
-);
+my $framework = PAGI::WebServer->new(config_file => 'config.yml');
 
 $framework->setup_logging;
 
-my $markdown = PAGI::WebServer::Markdown->new;
+my $markdown  = PAGI::WebServer::Markdown->new;
 my $pages_dir = path('share/pages');
 
 # Create router
 my $router = PAGI::WebServer::Router->new;
 
 # Add route for root
-$router->get('/' => async sub {
+$router->get(
+  '/' => async sub {
     my ($scope, $receive, $send) = @_;
 
     my $index_file = $pages_dir->child('index.md');
     if ($index_file->exists) {
-        my $html = $markdown->render($index_file->stringify);
-        my $bytes = encode_utf8($html);
+      my $html  = $markdown->render($index_file->stringify);
+      my $bytes = encode_utf8($html);
 
-        await $send->({
-            type => 'http.response.start',
-            status => 200,
-            headers => [['content-type', 'text/html; charset=utf-8']],
-        });
-        await $send->({
-            type => 'http.response.body',
-            body => $bytes,
-            more => 0,
-        });
-    } else {
-        await $send->({
-            type => 'http.response.start',
-            status => 404,
-            headers => [['content-type', 'text/plain']],
-        });
-        await $send->({
-            type => 'http.response.body',
-            body => 'Not Found',
-            more => 0,
-        });
+      await $send->({
+        type    => 'http.response.start',
+        status  => 200,
+        headers => [['content-type', 'text/html; charset=utf-8']],
+      });
+      await $send->({
+        type => 'http.response.body',
+        body => $bytes,
+        more => 0,
+      });
     }
-});
+    else {
+      await $send->({
+        type    => 'http.response.start',
+        status  => 404,
+        headers => [['content-type', 'text/plain']],
+      });
+      await $send->({
+        type => 'http.response.body',
+        body => 'Not Found',
+        more => 0,
+      });
+    }
+  }
+);
 
 # Add dynamic route for markdown files
-$router->get('*' => async sub {
+$router->get(
+  '*' => async sub {
     my ($scope, $receive, $send) = @_;
 
     my $path = $scope->{path};
-    $path =~ s|^/||; # Remove leading slash
+    $path =~ s|^/||;    # Remove leading slash
 
     # Try exact path first
     my $md_file = $pages_dir->child("$path.md");
 
     # If not found, try as directory with index.md
     if (!$md_file->exists) {
-        $md_file = $pages_dir->child($path, 'index.md');
+      $md_file = $pages_dir->child($path, 'index.md');
     }
 
     if ($md_file->exists) {
-        my $html = $markdown->render($md_file->stringify);
-        my $bytes = encode_utf8($html);
+      my $html  = $markdown->render($md_file->stringify);
+      my $bytes = encode_utf8($html);
 
-        await $send->({
-            type => 'http.response.start',
-            status => 200,
-            headers => [['content-type', 'text/html; charset=utf-8']],
-        });
-        await $send->({
-            type => 'http.response.body',
-            body => $bytes,
-            more => 0,
-        });
-    } else {
-        await $send->({
-            type => 'http.response.start',
-            status => 404,
-            headers => [['content-type', 'text/plain']],
-        });
-        await $send->({
-            type => 'http.response.body',
-            body => 'Not Found',
-            more => 0,
-        });
+      await $send->({
+        type    => 'http.response.start',
+        status  => 200,
+        headers => [['content-type', 'text/html; charset=utf-8']],
+      });
+      await $send->({
+        type => 'http.response.body',
+        body => $bytes,
+        more => 0,
+      });
     }
-});
+    else {
+      await $send->({
+        type    => 'http.response.start',
+        status  => 404,
+        headers => [['content-type', 'text/plain']],
+      });
+      await $send->({
+        type => 'http.response.body',
+        body => 'Not Found',
+        more => 0,
+      });
+    }
+  }
+);
 
 # Create and run server
 my $server = PAGI::Server->new(
-    app => $router->to_app,
-    port => 3001,
+  app  => $router->to_app,
+  port => 3001,
 );
 
 $server->run;
---
+++ lib/App/Schierer/HPFan.pm
@@ -0,0 +1,7 @@
+package App::Schierer::HPFan;
+use v5.42.0;
+use utf8::all;
+our $VERSION = 'v0.02.0';
+
+1;
+__END__
