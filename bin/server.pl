bc88fcfc338ee5a5afc6eef80852e826c129fa51
Luke Schierer <lschiere@amazon.com>
2025-12-31 20:28:02 -0500

    more closely match my intended url pattern

--- bin/server.pl
+++ bin/server.pl
@@ -3,9 +3,11 @@ use v5.42.0;
 use utf8::all;
 use Encode qw(encode_utf8);
 use lib '../PAGI-WebServer/lib';
+use lib 'lib';
 use PAGI::WebServer;
 use PAGI::WebServer::Router;
 use PAGI::WebServer::Markdown;
+use App::Schierer::HPFan::Person;
 use PAGI::Server;
 use Future::AsyncAwait;
 use Path::Tiny;
@@ -14,7 +16,8 @@ my $framework = PAGI::WebServer->new(config_file => 'config.yml');
 
 $framework->setup_logging;
 
-my $markdown  = PAGI::WebServer::Markdown->new;
+my $markdown = PAGI::WebServer::Markdown->new;
+my $person_handler = App::Schierer::HPFan::Person->new;
 my $pages_dir = path('share/pages');
 
 # Create router
@@ -55,6 +58,57 @@ $router->get(
     }
   }
 );
+
+# Add person routes (higher priority than markdown)
+$router->get('/Harrypedia/people/*' => async sub {
+    my ($scope, $receive, $send) = @_;
+    
+    my $path = $scope->{path};
+    my ($surname, $given_name) = $path =~ m{^/Harrypedia/people/([^/]+)/(.+)$};
+    
+    if ($surname && $given_name) {
+        # URL decode the names
+        $surname =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+        $given_name =~ s/%([0-9A-Fa-f]{2})/chr(hex($1))/eg;
+        
+        # Check for static markdown content for this person
+        my $static_content = '';
+        my $person_md = $pages_dir->child("Harrypedia", "people", $surname, "$given_name.md");
+        if ($person_md->exists) {
+            $static_content = $markdown->render($person_md->stringify);
+        }
+        
+        my $html = $person_handler->render_person_page($surname, $given_name, $static_content);
+        
+        if ($html) {
+            my $bytes = encode_utf8($html);
+            
+            await $send->({
+                type => 'http.response.start',
+                status => 200,
+                headers => [['content-type', 'text/html; charset=utf-8']],
+            });
+            await $send->({
+                type => 'http.response.body',
+                body => $bytes,
+                more => 0,
+            });
+            return;
+        }
+    }
+    
+    # Person not found
+    await $send->({
+        type => 'http.response.start',
+        status => 404,
+        headers => [['content-type', 'text/plain']],
+    });
+    await $send->({
+        type => 'http.response.body',
+        body => 'Person Not Found',
+        more => 0,
+    });
+});
 
 # Add dynamic route for markdown files
 $router->get(
