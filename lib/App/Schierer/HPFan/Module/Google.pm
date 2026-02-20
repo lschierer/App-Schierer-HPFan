9f429cc99615b4321f44ff25360623e687b6f1ad
Luke Schierer <lschiere@amazon.com>
2026-02-20 18:31:48 -0500

    build: insert google analytics in prod

---
+++ lib/App/Schierer/HPFan/Module/Google.pm
@@ -0,0 +1,31 @@
+package App::Schierer::HPFan::Module::Google;
+# cspell: disable 
+
+use v5.42.0;
+use utf8::all;
+use Mooish::Base -role;
+with 'WebFramework::Role::Logger';
+extends 'Thunderhorse::Module';
+use Future::AsyncAwait;
+
+
+use Mojo::DOM58;
+use HTML::Escape qw(escape_html);
+
+use Path::Tiny;
+
+
+
+
+const $googlescript = qq{<script async src="https://www.googletagmanager.com/gtag/js?id=G-9KF1R3YFTZ"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-9KF1R3YFTZ');
+    </script>}; 
+
+
+1;
+__END__   
