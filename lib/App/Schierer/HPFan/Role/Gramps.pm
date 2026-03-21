33e6cf4c5408121d2d3463cca09e2c8ed3163ac5
Luke Schierer <lschiere@amazon.com>
2026-03-20 20:38:09 -0400

    tidy

--- lib/App/Schierer/HPFan/Role/Gramps.pm
+++ lib/App/Schierer/HPFan/Role/Gramps.pm
@@ -17,8 +17,10 @@ has 'gramps_export' => (
 
 has 'gramps_db' => (
   is      => 'lazy',
-  default =>
-    sub { path($FindBin::Bin)->parent->child('share/gramps/grampsdb/potter_universe/sqlite.db') }
+  default => sub {
+    path($FindBin::Bin)
+      ->parent->child('share/gramps/grampsdb/potter_universe/sqlite.db');
+  }
 );
 
 my $g;
